8f5d0b01620ad5646d332c432d0c5bbd87463190
Kevin Griffin <kgriffin@cerego.com>
2015-01-26 17:13:27 +0900

    Apply to .js files as well

--- lib/browserify_pipeline/transformer/six_to_fiveify.rb
+++ lib/browserify_pipeline/transformer/six_to_fiveify.rb
@@ -4,7 +4,7 @@ module BrowserifyPipeline
   module Transformer
     class SixToFiveify < Base
 
-      def initialize(command_line_options = '--extensions .browserify')
+      def initialize(command_line_options = '--extensions .browserify --extensions .js')
         super('6to5ify', command_line_options)
       end
 
